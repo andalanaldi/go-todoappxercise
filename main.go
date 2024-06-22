5e70699cd195e7d22e9c39e028e5be3b861e1809
Aldi Andalan <andalanaldi@gmail.com>
2024-06-22 23:07:16 +0700

    refactor: API without DB completed-store in memory(1st part)

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 )
 
 type Todo struct {
@@ -24,8 +26,16 @@ func main()  {
 	// fmt.Println(myThirdName)
 	app := fiber.New()
 
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	PORT := os.Getenv("PORT")
+
 	todos := []Todo{}
 
+	// Get ToDos
 	app.Get("/api/todos", func(c *fiber.Ctx) error {
 		return c.Status((200)).JSON(todos) // formerly fiber.Map{"msg": "hello universe"}
 	})
@@ -70,9 +80,21 @@ func main()  {
 	})
 
 	// Delete a Todo
-	// app.Delete("/api/todos/:id", func(c *fiber.Ctx) error {
+	app.Delete("/api/todos/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
 
-	// })
+		for i, todo := range todos {
+			if fmt.Sprint(todo.ID) == id {
+				todos = append(todos[:i],todos[i+1:]...)
+				return c.Status(200).JSON(fiber.Map{"success": true})
+			}
+		}
 
-	log.Fatal(app.Listen(":4000"))
+		return c.Status(404).JSON(fiber.Map{"error": "Todo not found"})
+		// we have 5 values, each of them are 1 2 3 4 5, we delete one which is the 3rd, so it becomes:
+		// 1 2 4 5
+		// ... == periodic operator === spread operator in js to unpack the values
+	})
+
+	log.Fatal(app.Listen(":"+PORT)) // formerly ":4000" wo PORT err
 }
