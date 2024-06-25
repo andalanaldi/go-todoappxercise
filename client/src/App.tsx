602a65895d3c0b4240949c0ffc75675ef9989f27
Aldi Andalan <andalanaldi@gmail.com>
2024-06-25 19:11:56 +0700

    feat: add navbar,container,todos tags

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,19 @@
-import { Button } from '@chakra-ui/react';
+import { Container, Stack } from '@chakra-ui/react';
+import Navbar from './components/Navbar';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
 
 function App() {
 
   return (
-    <>
-      <Button>Hello</Button>
-    </>
+    <Stack h="100vh">
+      <Navbar />
+      <Container>
+        <TodoForm />
+        <TodoList />
+      </Container>
+      {/* <Button>Hello</Button> */}
+    </Stack>
   )
 }
 
