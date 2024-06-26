b05184cadf506e6c6a83cc1f2717aa19e2e66f43
Aldi Andalan <andalanaldi@gmail.com>
2024-06-27 04:08:49 +0700

    feat: add special BASE_URL line for production

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,13 @@ import Navbar from './components/Navbar';
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
 
-function App() {
+// Only use this line in production:
+// export const BASE_URL = import.meta.env.MODE === "development" ? "http://127.0.0.1:5000/api" : "/api"
 
+// Use this line in development:
+export const BASE_URL = "http://127.0.0.1:5000/api"
+
+function App() {
   return (
     <Stack h="100vh">
       <Navbar />
