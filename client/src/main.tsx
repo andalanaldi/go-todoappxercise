36d9357590b2bb8a1121bfe6adb6f64452079a4e
Aldi Andalan <andalanaldi@gmail.com>
2024-06-27 04:01:37 +0700

    feat: add TanStack queryClient wrapper

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import theme from './chakra/theme.ts'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider>
-    <App />
+    <QueryClientProvider client={queryClient}>
+    <ChakraProvider theme={theme}>
+      <App />
     </ChakraProvider>
-  </React.StrictMode>,
+    </QueryClientProvider>
+  </React.StrictMode>
 )
