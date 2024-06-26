89e0e2d5b6921f57b95320783e3ae2733578e613
Aldi Andalan <andalanaldi@gmail.com>
2024-06-27 04:05:11 +0700

    feat: drop hardcoded data, fetch API for TodoList

--- client/src/components/TodoList.tsx
+++ client/src/components/TodoList.tsx
@@ -1,31 +1,31 @@
 import { Flex, Spinner, Stack, Text } from "@chakra-ui/react";
-import { useState } from "react";
 import TodoItem from "./TodoItem";
+import { useQuery } from "@tanstack/react-query";
+
+
+export type Todo = {
+	_id: number;
+	body: string;
+	completed: boolean;
+}
 
 const TodoList = () => {
-	const [isLoading, setIsLoading] = useState(false);
-	const todos = [
-		{
-			_id: 1,
-			body: "Buy food",
-			completed: true,
-		},
-		{
-			_id: 2,
-			body: "Play the cat",
-			completed: false,
-		},
-		{
-			_id: 3,
-			body: "Do dishes",
-			completed: false,
-		},
-		{
-			_id: 4,
-			body: "Heat dinner",
-			completed: true,
-		},
-	];
+	const {data:todos, isLoading} = useQuery<Todo[]>({
+		queryKey:["todos"],
+		queryFn: async () => {
+			try {
+				const res = await fetch("http://127.0.0.1:5000/api/todos") //localhost
+				const data = await res.json()
+
+				if(!res.ok){
+					throw new Error(data.error || "Something went wrong")
+				}
+				return data || []
+			} catch (error) {
+		console.log(error)
+			}
+		}
+	})
 	return (
 		<>
 			<Text fontSize={"4xl"} textTransform={"uppercase"} fontWeight={"bold"} textAlign={"center"} my={2} bgGradient='linear(to-l, #0b85f8, #00ffff)'
@@ -46,8 +46,8 @@ const TodoList = () => {
 				</Stack>
 			)}
 			<Stack gap={3}>
-				{todos?.map((todo) => (
-					<TodoItem key={todo._id} todo={todo} />
+				{todos?.map((todo) => ( // (todo, idx) - for a temporary replace of _id
+					<TodoItem key={todo._id} todo={todo} /> // key={idx}
 				))}
 			</Stack>
 		</>
